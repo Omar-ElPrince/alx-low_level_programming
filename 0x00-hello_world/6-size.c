4896a3b09239143bfa2476736062266c0c018abb
Omar-ElPrince <omarelprince100@gamil.com>
2023-06-30 17:24:22 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
 
 int main(void)
 {
-printf("size of a %c\n", sizeof(char));
-printf("size of an %i\n", sizeof(int));
-printf("size of a %li\n", sizeof(long int));
-printf("size of a %lli\n", sizeof(long long int));
-printf("size of a %f\n", sizeof(float));
+printf("size of a char: %c byte(s)\n", sizeof(char));
+printf("size of an int: %i byte(s)\n", sizeof(int));
+printf("size of a long int: %li byte(s)\n", sizeof(long int));
+printf("size of a long long int: %lli byte(s)\n", sizeof(long long int));
+printf("size of a float: %f byte(s)\n", sizeof(float));
 return (0);
 }
